fecae1628c4b4161fef00a7092b8ca5f9ce816d9
marc-medley <marc-medley@users.noreply.github.com>
2019-08-11 15:41:19 -0700

    intial vapor commit

---
+++ CONTRIBUTING.md
@@ -0,0 +1,3 @@
+# Contributing to OpenSmartKitchen OSK-WebUI
+
+If you found a mistake or think of a cool new feature, please [create an issue](https://github.com/opensmartkitchen/OSK-WebUI/issues/new).
---
+++ OSKWebUI/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:4.2
+// swift-tools-version declares minimum Swift version to build this package.
+// Syntax: '// swift-tools-version:<specifier>' on the 1st line. (4.2|5.0)
+import PackageDescription
+
+let package = Package(
+    name: "OSKWebUI",
+    dependencies: [
+        // A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+
+        // An expressive, performant, and extensible templating language built for Swift.
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
+    ],
+    targets: [
+        .target(name: "App", dependencies: ["Leaf", "Vapor"]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App"])
+    ],
+    swiftLanguageVersions: [.v4_2] // [.v4_2, .v5]
+)
